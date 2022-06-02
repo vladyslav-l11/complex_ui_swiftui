f5f2459cee9ac119946609b84fba1ec45872c26c
vladyslav-l11 <88321655+vladyslav-l11@users.noreply.github.com>
2022-06-02 10:08:46 +0300

    init commit

---
+++ ComplexSwiftUI/ComplexSwiftUI/ComplexSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexSwiftUIApp.swift
+//  ComplexSwiftUI
+//
+//  Created by Vladyslav Lysenko on 02.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ComplexSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
