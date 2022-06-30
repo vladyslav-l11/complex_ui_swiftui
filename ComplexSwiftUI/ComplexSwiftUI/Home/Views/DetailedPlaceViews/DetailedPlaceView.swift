796e57c871a2c77a8dc86559eb51e9a29a77ed86
vladyslav-l11 <88321655+vladyslav-l11@users.noreply.github.com>
2022-06-30 19:49:52 +0300

    detail place view

---
+++ ComplexSwiftUI/ComplexSwiftUI/Home/Views/DetailedPlaceViews/DetailedPlaceView.swift
@@ -0,0 +1,52 @@
+//
+//  DetailedPlaceView.swift
+//  ComplexSwiftUI
+//
+//  Created by Vladyslav Lysenko on 28.06.2022.
+//
+
+import SwiftUI
+
+struct DetailedPlaceView: View {
+    
+    private enum C {
+        static let backButtonSize: CGFloat = 27
+    }
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    let place: Place
+    
+    var body: some View {
+        NavigationView {
+            GeometryReader { _ in
+                    Image(uiImage: place.image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: UIScreen.main.bounds.width,
+                               height: UIScreen.main.bounds.height*0.7,
+                               alignment: .center)
+                        .overlay(LinearGradient(gradient: Gradient(colors: [.black.opacity(0.5), .black.opacity(0.4), .black.opacity(0.3),         .black.opacity(0.3), .black.opacity(0.2), .black.opacity(0.1), .clear]), startPoint: .top, endPoint: .bottom)
+                                    .frame(height: 80),
+                                    alignment: .top)
+            }
+            .ignoresSafeArea()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Image(uiImage: UIImage(named: "icArrowLeft").nonNil)
+                    }.frame(width: C.backButtonSize, height: C.backButtonSize)
+                }
+            }
+            .overlay(PlaceInfoView(place: place), alignment: .bottom)
+        }
+    }
+}
+
+struct DetailedPlaceView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailedPlaceView(place: Place(name: "Name1", country: "Country1", city: "City1", image: UIImage(named: "icWood").nonNil, distance: 6, description: "descdghsfjhsdgfjhsdgfjhsdfghjdsgfhdsgfhjsdgfjhdsgfhjsdgfhjdsgjfhsdghjfgsdhjfsgdhjfgsdhjfgsdhjfgsdhjgfsdjhfgsdjhfghjsdgfjdhsgfsdhjgfhjsdgfhjsdgfjhsdgfhjsdghjfgsdjhfgdshgfhjsdgfhjsdgfjhsdghjfsdghjfgdshjfgsdhjfgsdhjfgshjdfgsjhfsgjhfdsghjfgsdhjfgsdjhfgsdhjfgdshjfgsdhjgfsdhjfgsjhfgsdhjfgdsjhrjdfshkjfdshkjfhdsjkfhsdjkhfjsdhfjksdhjfhsdkjfhsdkjfhsdjhfkjsdhfjsdhfkjsdhfjksdhfkjsdhfkjhsdjkfhsdkjfhsdkjhfkjsdhjfsdjfhsdjkfhdskjfhsdjkfhjksdfhjksdfhjkdsfhsdjkfhsdjhfhgdshjfgsdhjfgsjhfgsdhfgsdhfgshfgsdhjfgsdhjfgsdjhfgsdhjfgsdhjgfsdjhfgsdjhgfjhsdgfjhsdgfhjsdgfhjsdghjfgsdjhfgsdhjfgsdhjfgsdhjfgsjdhgfshdjfgshjdfgsdhjfgshjdgfhjsdgfhjsdgfhjsdgfhjgsdhjfgsdhjfgsdjhfgsdhjfghjsdfgsdhjfgsdhjgfsdhjgfsdhjgfhsdgfhsdgfjhsdgfjhsdgfjhsdgfhjdsghjfgsdhjfgsdhjfgsdhjfgshfgsdhfsdghfsdghfjsdgfhsdgfhjsdgfhsdgfhsdgfdshjfgshjdfgsdhjfgsdhjfgdshjfghdjsgfsdhjgfhjsdgfhjsdgfhsdgfhsdjfgshjdgsfjhsdgfhjsdgfhsdgfhjsdgfhjsdgfhdsgfhdsjgfsdhjfgyyyyyeyyeyeyfsdbjhfbsdhjfbshjbfsdbfhjsbdjhfbdshfbhsdjbfhjsdbfhjbsdhjfsdbhfbdshjfbsdhfbshdfbjhsdfbjsdbfsjhdkfbsdjhbfhjsdbfjhsdbfhjsdbhjfbsjhbfhjsdbhfjsdhjfsbfhjsbfjhdbfjhsbhjfdsbjhfsbdjhfbsdjhfbsdhbfjhsdbfjsdjfhsbdjhfsbdjhfbsdfbsdhfbsjhsdbfhjsdbfjhsdbfjhsdbfhjsdbfhjbsdjhfbshjdbfjhsdbfhjsbhfbsdjhfbsdhfbsjhbdfhdbshjfsdbhfsbjhfbshjfdbjhfsbdjfhsdbjhfbshfbsdjhbsfhjdsbhjfbdshjfsbhjfbsjhfbdhsdbfhsdbjfhbsdhfbsdjhfbsdhjfbshjbfsdhbfshfbdsjhfbshdfbhsdfbjsdhfbsdhfbsdhfbhsdjfbsdhfbsdhfjsdbfhsjdbfhsdjbfhbjsdfkjshdfjhsdfjshdjfhsdjfkhsdfhjsdhfkjsdhfjsdhfjksdhkfjsdjfhsjdkfsdjfshdkjfhsjkfhskjdsfjkdsfhkj", hotelsCount: 8, restaurantsCount: 7))
+    }
+}
---
+++ ComplexSwiftUI/ComplexSwiftUI/Home/Views/DetailedPlaceViews/PlaceInfoView.swift
@@ -0,0 +1,95 @@
+//
+//  PlaceInfoView.swift
+//  ComplexSwiftUI
+//
+//  Created by Vladyslav Lysenko on 28.06.2022.
+//
+
+import SwiftUI
+
+struct PlaceInfoView: View {
+    
+    private enum C {
+        static let cornerRadius: CGFloat = 45
+        static let blurRadius: CGFloat = 3
+    }
+    
+    let place: Place
+    
+    var body: some View {
+        Group {
+            VStack(alignment: .leading) {
+                Text(place.name)
+                    .font(.custom("Metropolis-Black", size: 24))
+                    .foregroundColor(Color(UIColor(named: "gray1") ?? .init()))
+                    .frame(alignment: .leading)
+                    .padding(.bottom, 5)
+                Text("\(place.city), \(place.country)")
+                    .font(.custom("Metropolis-SemiBold", size: 13))
+                    .foregroundColor(Color(UIColor(named: "gray1") ?? .init()))
+                    .padding(.bottom, 11)
+                ScrollView {
+                    Text(place.description)
+                        .font(.custom("Metropolis-SemiBold", size: 14))
+                        .foregroundColor(Color(UIColor(named: "gray3") ?? .init()))
+                        .lineLimit(nil)
+                }
+                    
+                
+                HStack(spacing: 14) {
+                    Group {
+                        Text("\(place.hotelsCount) Hotels")
+                            .font(.custom("Metropolis-Medium", size: 14))
+                            .foregroundColor(Color(UIColor(named: "gray1") ?? .init()))
+                            .padding(.horizontal, 14)
+                            .padding(.vertical, 11)
+                            .background(RoundedRectangle(cornerRadius: 14).foregroundColor(Color(uiColor: UIColor(named: "blueTen1") ?? .init())))
+                    }
+                    .frame(width: .infinity, height: 37)
+                    Group {
+                        Text("\(place.restaurantsCount) Restaurants")
+                            .font(.custom("Metropolis-Medium", size: 14))
+                            .foregroundColor(Color(UIColor(named: "gray1") ?? .init()))
+                            .padding(.horizontal, 14)
+                            .padding(.vertical, 11)
+                            .background(RoundedRectangle(cornerRadius: 14).foregroundColor(Color(uiColor: UIColor(named: "blueTen1") ?? .init())))
+                    }
+                    .frame(width: .infinity, height: 37)
+                }
+                .padding(.leading, -6)
+                .padding(.vertical, 12)
+                
+                HStack {
+                    Image(uiImage: UIImage(named: "icPointer") ?? .init())
+                        .frame(width: 20, height: 20)
+                    Text("\(place.distance) Km from here")
+                        .font(.custom("Metropolis-SemiBold", size: 16))
+                        .foregroundColor(Color(UIColor(named: "gray2") ?? .init()))
+                }.padding(.vertical, 4)
+                
+                Button(action: {}) {
+                    Text("Book Flight for My Trip")
+                        .font(.custom("Metropolis-SemiBold", size: 17))
+                        .foregroundColor(.white)
+                }
+                .frame(width: UIScreen.main.bounds.width - 40, height: 67)
+                    .background(RoundedRectangle(cornerRadius: 17).foregroundColor(Color(uiColor: UIColor(named: "blue1").nonNil)))
+                    .padding(.top, 10)
+                    .padding(.horizontal, -4)
+            }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 28)
+        }
+        .frame(width: UIScreen.main.bounds.width,
+                height: .infinity,
+                alignment: .leading)
+        .background(RoundedCorners(cornerRadius: C.cornerRadius, corners: [.topRight, .topLeft])
+                        .foregroundStyle(.thinMaterial))
+    }
+}
+
+struct PlaceInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaceInfoView(place: Place(name: "Name1", country: "Country1", city: "City1", image: UIImage(named: "icWood").nonNil, distance: 6, description: "descdghsfjhsdgfjhsdgfjhsdfghjdsgfhdsgfhjsdgfjhdsgfhjsdgfhjdsgjfhsdghjfgsdhjfsgdhjfgsdhjfgsdhjfgsdhjgfsdjhfgsdjhfghjsdgfjdhsgfsdhjgfhjsdgfhjsdgfjhsdgfhjsdghjfgsdjhfgdshgfhjsdgfhjsdgfjhsdghjfsdghjfgdshjfgsdhjfgsdhjfgshjdfgsjhfsgjhfdsghjfgsdhjfgsdjhfgsdhjfgdshjfgsdhjgfsdhjfgsjhfgsdhjfgdsjhr", hotelsCount: 8, restaurantsCount: 7))
+    }
+}
